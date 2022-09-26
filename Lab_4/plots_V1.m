8537f475086573715de3dfcbc71e9ea18c3b5b2b
rohxnsxngh <98181667+rohxnsxngh@users.noreply.github.com>
2022-09-25 22:38:19 -0500

    added plots lab4

--- Lab_4/plots_V1.m
+++ Lab_4/plots_V1.m
@@ -1,4 +1,26 @@
-load("data_v1.mat");
 figure;
-plot(omega.time, omega.signals.values);
+load("data_v1.mat");
+plot(omega.time, omega.signals.values,'DisplayName','Voltage 1');
+hold on
+load("data_v1.5.mat");
+plot(omega.time, omega.signals.values,'DisplayName','Voltage 1.5');
+hold on
+load("data_v2.mat");
+plot(omega.time, omega.signals.values,'DisplayName','Voltage 2');
+hold on
+load("data_v2.5.mat");
+plot(omega.time, omega.signals.values,'DisplayName','Voltage 2.5');
+hold on
+load("data_v3.mat");
+plot(omega.time, omega.signals.values,'DisplayName','Voltage 3');
+hold on
+load("data_v3.5.mat");
+plot(omega.time, omega.signals.values,'DisplayName','Voltage 3.5');
+hold off
+title('Angular Velocity vs Time(s)')
+xlabel('Time(s)') 
+ylabel('Angular Velocity') 
 legend;
+
+
+
